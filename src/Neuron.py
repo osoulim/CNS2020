34feb28d2c0f2633b9858e54d6f246edfc1eba67
MamZi <mohammadreza.osouli>
2020-05-08 18:05:26 +0430

    Reformat files

--- src/Neuron.py
+++ src/Neuron.py
@@ -1,21 +1,20 @@
-
 class Neuron:
 
-	def __init__(self):
-		self.threshold = 25
-		self.tm = 2500
-		self.current_v = 0
-		self.last_update_time = 0
-		self.spike_time = -1
+    def __init__(self):
+        self.threshold = 25
+        self.tm = 2500
+        self.current_v = 0
+        self.last_update_time = 0
+        self.spike_time = -1
 
-	def spike(self):
-		self.current_v = 0
-		
-	def check_spike(self):
-		return self.current_v > self.threshold
+    def spike(self):
+        self.current_v = 0
 
-	def excite(self, v):
-		self.current_v += v
+    def check_spike(self):
+        return self.current_v > self.threshold
 
-	def update(self, t):
-		self.last_update_time = t
+    def excite(self, v):
+        self.current_v += v
+
+    def update(self, t):
+        self.last_update_time = t
--- src/Synapse.py
+++ src/Synapse.py
@@ -1,8 +1,7 @@
-
 class Synapse:
 
-	def __init__(self):
-		self.weight = 0
-		self.delay = 0
-		self.pre_neurons = list()
-		self.post_neurons = list()
+    def __init__(self):
+        self.weight = 0
+        self.delay = 0
+        self.pre_neurons = list()
+        self.post_neurons = list()
