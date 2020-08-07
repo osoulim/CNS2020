48b63af8c2bdd29b2121d1941101d68741dd3209
MamZi <mohammadreza.osouli>
2020-08-08 02:03:10 +0430

    Fix dataset

--- Final/dataset.py
+++ Final/dataset.py
@@ -4,7 +4,7 @@ import random
 
 
 class Dataset:
-    def __init__(self, dataset_directory, subjects, image_size=(30, 30), filters=None):
+    def __init__(self, dataset_directory, subjects, image_size=(30, 30), filters=[]):
         self.data = []
         for index, subject in enumerate(subjects):
             for image_name in gb.glob("%s/%s/*.jpg" % (dataset_directory, subject)):
@@ -14,17 +14,24 @@ class Dataset:
                     img = image_filter(img)
                 self.data.append((img, index))  # (image, label)
 
-    def get_data(self, train_percentage=0.7):
+    def get_data(self, filters, train_percentage=0.7):
         random.shuffle(self.data)
-        train_size = int(len(self.data) * train_percentage)
-        train_data, train_label = [[i for i, j in self.data[0: train_size]], [j for i, j in self.data[0: train_size]]]
-        test_data, test_label = [[i for i, j in self.data[train_size:]], [j for i, j in self.data[train_size:]]]
+        batch_data = []
+        for image, label in self.data:
+            temp = []
+            for image_filter in filters:
+                temp.append(image_filter(image))
+            batch_data.append((temp, label))
+
+        train_size = int(len(batch_data) * train_percentage)
+        train_data, train_label = [[i for i, j in batch_data[0: train_size]], [j for i, j in batch_data[0: train_size]]]
+        test_data, test_label = [[i for i, j in batch_data[train_size:]], [j for i, j in batch_data[train_size:]]]
         return train_data, train_label, test_data, test_label
 
 
 if __name__ == "__main__":
     bgr2gray = lambda x: cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-    dataset = Dataset('data', ['ant', 'camera'], (100, 100), (bgr2gray, ))
-    cv2.imshow("sag", dataset.get_data()[2][0])
+    dataset = Dataset('data', ['ant', 'camera'], (100, 100))
+    cv2.imshow("sag", dataset.get_data([bgr2gray, ])[2][1][0])
     cv2.waitKey(0)
     cv2.destroyAllWindows()
